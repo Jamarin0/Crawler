29408eed1b7ebd6c0e654d6ee6cabdcc35c2c77b
jamarin0 <jgjsguilherme@gmail.com>
2021-01-20 16:20:18 -0300

    fex: add link to results

--- searchGoogle.js
+++ searchGoogle.js
@@ -10,8 +10,8 @@ const searchGoogle = async (searchQuery = 'carro') => {
    
     await page.type('input[name=q]', searchQuery, { delay: 100 })
     await page.click('input[type="submit"]')
-    await page.waitForSelector('h3 span')
-    const links = await page.$$eval('h3 span', anchors => { return anchors.map(a => { return a.textContent }) })
+    await page.waitForSelector('div[id="search"] a')
+    const links = await page.$$eval('div[id="search"] a', anchors => { return anchors.map(a => { return a.href }) })
 
     
     await browser.close();
